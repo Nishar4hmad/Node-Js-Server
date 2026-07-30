1d1e536f6d528cc75816fcb9d877c51c5fb2d5a5
Nishar Ahmad <nishar4hmad@gmail.com>
2026-07-31 03:23:25 +0530

    added more pages and get method

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+  const API = {
+    success: true,
+    message: "Welcome to my first API",
+  };
 
-  if (url === "/") {
+  if (url === "/" && method === "GET") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("Welcome to my Node Js Server");
-  } else if (url === "/about") {
+  } else if (url === "/api" && method === "GET") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify(API));
+  } else if (url === "/about" && method === "GET") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("About Page");
-  } else if (url === "/contact") {
+    res.end("About page");
+  } else if (url === "/contact" && method === "GET") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("Contact Page");
   } else {
@@ -19,6 +27,6 @@ const server = http.createServer((req, res) => {
 });
 
 const PORT = 3000;
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log(`Server is running at :${PORT}`);
 });
